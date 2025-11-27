eb754b668cb0b69897e0ff7cf7bcf2d7d7847a90
Sankhyahrick Swami <sankhyahricks@gmail.com>
2025-11-27 12:20:27 +0530

    fix

--- main.go
+++ main.go
@@ -68,7 +68,6 @@ func refreshAccessToken(email string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	fmt.Println("RefreshToken : ", body.RefreshToken)
 	var newBody struct {
 		AccessToken string `json:"access_token"`
 		Error       string `json:"error"`
